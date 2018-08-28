baad40ace2340aed3038ea699ab39fc00b02d50e
Shane Lupton <slupton89@gmail.com>
2018-08-28 12:35:27 -0600

    completed implementing youtube search requests

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,19 @@
-const API_KEY = 'AIzaSyDseuhc-4nMYQWWYlT1kz_Bnt8bY9eAq2U';
+const API_KEY = 'AIzaSyD-J6MV7HLE6pazoxRxDb48jvXcr5L6WCw';
 
-/*
+{/*
   We want our store to hold an array of "decorated" video objects - i.e. objects that
-  have been transformed into ONLY the necessary data we're displaying on our page. 
+  have been transformed into ONLY the necessary data we're displaying on our page.
   (Removes extraneous dataset from Youtube.)
-  
+
   Example decorated video object:
-  
+
   {
     id: '98ds8fbsdy67',
     title: 'Cats dancing the Macarena',
     thumbnail: 'https://img.youtube.com/some/thumbnail.jpg'
   }
 */
+}
 const store = {
   videos: []
 };
@@ -21,7 +22,8 @@ const store = {
 // Documentation is here: https://developers.google.com/youtube/v3/docs/search/list#usage
 const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-/**
+
+{/**
  * @function fetchVideos
  * Async function, responsible for calling the Youtube API with jQuery, constructing
  * the correct query object, and passing along the callback into the AJAX call.
@@ -31,41 +33,52 @@ const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 // TASK:
 // 1. Use `searchTerm` to construct the right query object based on the Youtube API docs
 //    - Refer to curriculum assignment for help with the required parameters
-// 2. Make a getJSON call using the query object and sending the provided callback in 
+// 2. Make a getJSON call using the query object and sending the provided callback in
 //    as the last argument
 //
 // TEST IT! Execute this function and console log the results inside the callback.
+}
+
+
 const decorateResponse = function(response) {
-  const results = response.items;
-  const decorated = [];
-  results.forEach((elem) => {
-    decorated.push({
-      id: elem.id.videoId,
-      title: elem.snippet.title,
-      thumbnail: elem.snippet.thumbnails.default.url
-    });
+  return response.items.map(item => {
+    return {
+      id: item.id.videoId,
+      title: item.snippet.title,
+      thumbnail: item.snippet.thumbnails.medium.url,
+    };
   });
-  store.videos = decorated;
-};
-const fetchVideos = function(searchTerm, callback) { 
-  const query = { 
-    key: API_KEY,
-    q: searchTerm, 
-    part: 'snippet', } 
-  $.getJSON(BASE_URL, query, callback); 
+  // const results = response.items;
+  // const decorated = [];
+  // results.forEach((elem) => {
+  //   decorated.push({
+  //     id: elem.id.videoId,
+  //     title: elem.snippet.title,
+  //     thumbnail: elem.snippet.thumbnails.default.url
+  //   });
+  // });
 };
 
-/**
+
+const fetchVideos = function(searchTerm, callback) {
+  const query = {
+    key: API_KEY,
+    q: searchTerm,
+    part: 'snippet', }
+  return $.getJSON(BASE_URL, query, callback);
+};
+
+{/**
  * @function decorateResponse
- * Uses Youtube API response to create an array of "decorated" video objects as 
+ * Uses Youtube API response to create an array of "decorated" video objects as
  * defined at the top of the file.
  * @param   {object} response - should match Youtube API response shape
  * @returns {array}
  */
 // TASK:
 // 1. Map through the response object's `items` array
-// 2. Return an array of objects, where each object contains the keys `id`, `title`, 
-//    `thumbnail` which each hold the appropriate values from the API item object. You 
+// 2. Return an array of objects, where each object contains the keys `id`, `title`,
+//    `thumbnail` which each hold the appropriate values from the API item object. You
 //    WILL have to dig into several nested properties!
 //
 // TEST IT! Grab an example API response and send it into the function - make sure
@@ -76,16 +89,24 @@ const fetchVideos = function(searchTerm, callback) {
  * @function generateVideoItemHtml
  * Template function, creates an HTML string from a single decorated video object
  * @param   {object} video - decorated video object
- * @returns {string} HTML 
+ * @returns {string} HTML
  */
 // TASK:
 // 1. Using the decorated object, return an HTML string containing all the expected
 // TEST IT!
-const generateVideoItemHtml = function(video) {
+}
 
+
+const generateVideoItemHtml = function(video) {
+    return `
+    <li data-id="${video.id}">
+      <h3>${video.title}</h3>
+      <img src="${video.thumbnail}">
+    </li>
+  `;
 };
 
-/**
+{/**
  * @function addVideosToStore
  * Store modification function to set decorated video objects
  * @param {array} videos - decorated video objects
@@ -93,12 +114,13 @@ const generateVideoItemHtml = function(video) {
 // TASK:
 // 1. Set the received array as the value held in store.videos
 // TEST IT!
+}
 const addVideosToStore = function(videos) {
-
+  store.videos = videos;
 };
 
 
-/**
+{/**
  * @function render
  * Responsible for scanning store and rendering the video list to DOM
  */
@@ -106,11 +128,13 @@ const addVideosToStore = function(videos) {
 // 1. Map through `store.videos`, sending each `video` through `generateVideoItemHtml`
 // 2. Add this array of DOM elements to the appropriate DOM element
 // TEST IT!
+}
 const render = function() {
-
+  const htmlElements = store.videos.map(generateVideoItemHtml);
+  $('.results').html(htmlElements);
 };
 
-/**
+{/**
  * @function handleFormSubmit
  * Adds form "submit" event listener that 1) initiates API call, 2) modifies store,
  * and 3) invokes render
@@ -122,16 +146,31 @@ const render = function() {
 //   c) Clear the search input field
 //   d) Invoke the `fetchVideos` function, sending in the search value
 //   e) Inside the callback, send the API response through the `decorateResponse` function
-//   f) Inside the callback, add the decorated response into your store using the 
+//   f) Inside the callback, add the decorated response into your store using the
 //      `addVideosToStore` function
-//   g) Inside the callback, run the `render` function 
+//   g) Inside the callback, run the `render` function
 // TEST IT!
+}
 const handleFormSubmit = function() {
+  $('#js-search-form').submit(event => {
+    event.preventDefault();
 
-};
+
+    const query = $('#search-input').val();
+    console.log('query', query);
+    // clear out the input
+    fetchVideos(query, response => {
+      const decoratedVideos = decorateResponse(response);
+      addVideosToStore(decoratedVideos);
+      render();
+    });
+  });
+}
+
 
 // When DOM is ready:
 $(function () {
   // TASK:
   // 1. Run `handleFormSubmit` to bind the event listener to the DOM
+  handleFormSubmit();
 });
